4a1703e4820ee925865b72d26d85dbc889ec4316
Khloudibrahem <Khloudk130@gmail.com>
2023-12-16 14:54:03 -0800

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,6 +7,29 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp;
+	listint_t *temper;
+	listint_t *node_to_swap;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	node_to_swap = (*list)->next;
+	while (node_to_swap)
+	{
+		temper = node_to_swap;
+		while (temper->prev && temper->n < temper->prev->n)
+		{
+			temper->prev->next = temper->next;
+			if (temper->next != NULL)
+				temper->next->prev = temper->prev;
+			temper->next = temper->prev;
+			temper->prev = temper->prev->prev;
+			temper->next->prev = temper;
+			if (temper->prev == NULL)
+				*list = temper;
+			else
+				temper->prev->next = temper;
+			print_list(*list);
+		}
+		node_to_swap = node_to_swap->next;
+	}
 }
