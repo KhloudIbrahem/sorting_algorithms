1a0c1b3db50fc4f586d591a8c40aeed9c4507162
Khloudibrahem <Khloudk130@gmail.com>
2023-12-18 16:27:11 -0800

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,4 +8,30 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t x;
+	size_t y;
+	size_t first;
+	int temper;
+
+	if (size == 0 || array == NULL)
+		return;
+	for (x = 0; x < size - 1; x++)
+	{
+		first = x;
+		for (y = x + 1; y < size; y++)
+		{
+			if (array[y] < array[first])
+			{
+				first = y;
+			}
+
+		}
+		if (first != x)
+		{
+			temper = array[first];
+			array[first] = array[x];
+			array[x] = temper;
+			print_array(array, size);
+		}
+	}
 }
